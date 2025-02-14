2cc75fb88b50e30592880c19b35f235c18d800b6
Sta1400 <1400sta@gmail.com>
2025-02-14 15:47:29 +0100

    remove magic number

--- script/DeployUSDTBPriceFeed.s.sol
+++ script/DeployUSDTBPriceFeed.s.sol
@@ -12,11 +12,14 @@ contract DeployUSDTBPriceFeedScript is Script {
     IPyth public pyth;
     bytes32 public usdtbPriceId;
 
+    address internal constant _PYTH_MAINNET_ORACLE = 0x4305FB66699C3B2702D4d05CF36551390A4c69C6;
+    bytes32 internal constant _USDTB_PRICE_ID = 0x967549f1ff4869f41cb354a7116b9e5a9a3091bebe0b2640eeed745ca1f7f90b;
+
     function run() external {
         address deployer_ = vm.rememberKey(vm.envUint("PRIVATE_KEY"));
 
-        pyth = IPyth(0x4305FB66699C3B2702D4d05CF36551390A4c69C6);
-        usdtbPriceId = bytes32(0x967549f1ff4869f41cb354a7116b9e5a9a3091bebe0b2640eeed745ca1f7f90b);
+        pyth = IPyth(_PYTH_MAINNET_ORACLE);
+        usdtbPriceId = _USDTB_PRICE_ID;
 
         console2.log("Deployer:", deployer_);
 
